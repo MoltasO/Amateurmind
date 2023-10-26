ba21fdabefe9002821d96a07ecb0dd86ba9da52b
MoltasO <moltas.sjoblom@gmail.com>
2023-10-26 20:59:38 +0200

    color

---
+++ color.py
@@ -0,0 +1,17 @@
+#https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html#256-colors
+#https://gist.github.com/rene-d/9e584a7dd2935d0f461904b9f2950007
+#https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+
+class ANSI():
+    def background(code):
+        return "\33[{code}m".format(code=code)
+ 
+    def style_text(code):
+        return "\33[{code}m".format(code=code)
+ 
+    def color_text(code):
+        return "\33[{code}m".format(code=code)
+ 
+ 
+example_ansi = ANSI.background(97) + ANSI.color_text(35) + ANSI.style_text(4) + " TESTE ANSI ESCAPE CODE"
+print(example_ansi)
